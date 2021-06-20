0c6f84edfe3a42d74a2844f161e39a0a70f310d7
hamuyatti <syosatc@i>
2021-06-20 21:28:25 +0900

    navigation追加

---
+++ app/src/main/java/com/example/mytodo/page/create/createToDoFragment.kt
@@ -0,0 +1,8 @@
+package com.example.mytodo.create
+
+import androidx.fragment.app.Fragment
+import com.example.mytodo.R
+
+class createToDoFragment: Fragment(R.layout.create_todo_fragment){
+
+}
---
+++ app/src/main/java/com/example/mytodo/page/edit/EditTodoFragment.kt
@@ -0,0 +1,7 @@
+package com.example.mytodo.page.edit
+
+import androidx.fragment.app.Fragment
+import com.example.mytodo.R
+
+class EditTodoFragment : Fragment(R.layout.edit_todo_fragment){
+}
